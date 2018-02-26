49e21f93c48283a952ac61da203272bff85a2503
jugbeep <patrickbotkins@gmail.com>
2018-02-26 14:46:29 -0700

    new images

--- src/pages/splash/tutorial.ts
+++ src/pages/splash/tutorial.ts
@@ -32,12 +32,12 @@ export class TutorialPage {
           {
             title: values.TUTORIAL_SLIDE1_TITLE,
             description: values.TUTORIAL_SLIDE1_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-1.png',
+            image: 'assets/img/NoteSomm.png',
           },
           {
             title: values.TUTORIAL_SLIDE2_TITLE,
             description: values.TUTORIAL_SLIDE2_DESCRIPTION,
-            image: 'assets/img/ica-slidebox-img-3.png',
+            image: 'assets/img/NoteSomm.png',
           }
         ];
       });
